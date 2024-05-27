6d875b455165cf7353c87f281226fbebcf83f0e9
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-27 11:23:11 -0700

    On to part three. But first, breakfast

--- script.mjs
+++ script.mjs
@@ -1,4 +1,4 @@
-// TODO: Part One,
+// TODO: Part One - Humble Beginnings,
 const adventurer = {
     name: "Robin", 
     health: 10, 
@@ -10,7 +10,7 @@ const adventurer = {
     felineFriend: {
         name: "Frank", 
         type: "Flea", 
-        inventory: ["A small Hat", "Sunglasses"]
+        inventory: ["Small hat", "Sunglasses"]
     }, 
     roll (mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
@@ -24,5 +24,32 @@ for(let i = 0; i < adventurer.inventory.length; i++) {
 
 adventurer.roll();
 
-// TODO: Part Two,
+// TODO: Part Two - Class Fantasy,
+
+// Each object has a name. felineFriend and Robin both have inventories. type describes what the object is, in this instance they are a cat and flea.
+
+class Character {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+    }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.felineFriend = new Character("Frank");
+robin.companion.felineFriend.type = "Flea";
+robin.companion.felineFriend.inventory = ["Small hat", "Sunglasses"];
+
+robin.companion.felineFriend.roll();
+robin.companion.roll();
+
+// TODO: Part Three - Class Features,
 
