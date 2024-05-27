3eb669f8bb8ca1aaed9e98129e03cd8f539a112e
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-27 14:50:14 -0700

    Finally on to part four

--- script.mjs
+++ script.mjs
@@ -1,24 +1,24 @@
 // TODO: Part One - Humble Beginnings,
 const adventurer = {
-    name: "Robin", 
-    health: 10, 
-    inventory: ["sword", "potion", "artifact"], 
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
     companion: {
-        name: "Leo", 
+        name: "Leo",
         type: "Cat"
-    }, 
+    },
     felineFriend: {
-        name: "Frank", 
-        type: "Flea", 
+        name: "Frank",
+        type: "Flea",
         inventory: ["Small hat", "Sunglasses"]
-    }, 
-    roll (mod = 0) {
+    },
+    roll(mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`);
     }
 };
 
-for(let i = 0; i < adventurer.inventory.length; i++) {
+for (let i = 0; i < adventurer.inventory.length; i++) {
     console.log(adventurer.inventory[i]);
 }
 
@@ -29,12 +29,12 @@ adventurer.roll();
 // Each object has a name. felineFriend and Robin both have inventories. type describes what the object is, in this instance they are a cat and flea.
 
 class Character {
-    constructor (name) {
+    constructor(name) {
         this.name = name;
         this.health = 100;
         this.inventory = [];
     }
-    roll (mod = 0) {
+    roll(mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`);
     }
@@ -52,4 +52,51 @@ robin.companion.felineFriend.roll();
 robin.companion.roll();
 
 // TODO: Part Three - Class Features,
+
+class Adventurer extends Character {
+    constructor(name, role) {
+        super(name);
+        // Adventurers have specialized rolls
+        this.role = role;
+        // Every adcenturer starts with a bed and 50 gold coins.
+        this.inventory.push("bedroll", "50 gold coins");
+        this.weapons = ["wrench"];
+        this.healthPotions = 5;
+        this.level = 13;
+        this.xP = 130;
+        this.diamondsCollected = 0;
+
+
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout() {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
+    }
+    // Adventurers are able to collect Diamonds.
+    collectDiamonds(numDiamonds) {
+        this.diamondsCollected += numDiamonds;
+        console.log(`${this.name} has encountered ${numDiamonds} diamonds and collected it`);
+    }
+}
+
+class Companion extends Character {
+    constructor(name, type) {
+        super(name);
+        this.type = type;
+    }
+}
+
+    const robinAdventurer = new Adventurer("Robin", "Sniper"); // Adventurer class
+    const leo = new Companion("Leo", "Cat"); // Companion Class
+    const frank = new Companion("Frank", "Flea"); // Companion Class
+
+    robinAdventurer.companion = leo;
+    robinAdventurer.companion.felineFriend = frank;
+    
+    console.log(robinAdventurer);
+    console.log(robinAdventurer.companion);
+    console.log(robinAdventurer.companion.felineFriend);
+
+// TODO: Part Four - Class Uniforms,
 
