b94690d7176a23fe0b4ef60efb495a5cb75423d6
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-27 10:50:48 -0700

    added a dice roll function

--- script.mjs
+++ script.mjs
@@ -5,6 +5,15 @@ const adventurer = {
     companion: {
         name: "Leo", 
         type: "Cat"
+    }, 
+    felineFriend: {
+        name: "Frank", 
+        type: "Flea", 
+        inventory: ["A small Hat", "Sunglasses"]
+    }, 
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
     }
 };
 
