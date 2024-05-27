2ee651f6d6d1713e6a5b7e8b19d84427915c54c7
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-27 10:35:49 -0700

    added for loop and a furry companion

--- script.mjs
+++ script.mjs
@@ -1,6 +1,13 @@
 const adventurer = {
     name: "Robin", 
     health: 10, 
-    inventory: ["sword", "potion", "artifact"]
+    inventory: ["sword", "potion", "artifact"], 
+    companion: {
+        name: "Leo", 
+        type: "Cat"
+    }
 };
 
+for(let i = 0; i < adventurer.inventory.length; i++) {
+    console.log(adventurer.inventory[i]);
+}
